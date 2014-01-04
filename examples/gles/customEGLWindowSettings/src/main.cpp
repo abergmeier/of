2832d56628b34a024d2c2e41199a2d9fc6a61d44
Andreas Bergmeier <abergmeier@gmx.net>
2014-01-04 11:55:47 +0100

    Add Emscripten build of customEGLWindowSettings example.

--- examples/gles/customEGLWindowSettings/src/main.cpp
+++ examples/gles/customEGLWindowSettings/src/main.cpp
@@ -1,19 +1,44 @@
 #include "ofMain.h"
 #include "ofApp.h"
+#ifdef TARGET_EMSCRIPTEN
+#include "EGLPage.hpp"
+using EGLWindow = of::emscripten::EGLPage;
+namespace {
+	constexpr const auto WINDOW_MODE = ofWindowMode::OF_WINDOW;
+
+	EGLWindow::Settings
+	create_settings( unsigned short opacity ) {
+		auto settings = EGLWindow::Settings{};
+		settings.eglCanvasOpacity = opacity;
+		return settings;
+	}
+}
+#else
 #include "ofAppEGLWindow.h"
+using EGLWindow = ofAppEGLWindow;
+namespace {
+	constexpr const auto WINDOW_MODE = ofWindowMode::OF_FULLSCREEN;
+
+	EGLWindow::Settings
+	create_settings( unsigned short opacity ) {
+		auto settings = EGLWindow::Settings{};
+		settings.eglWindowOpacity = opacity;
+		return settings;
+	}
+}
+#endif
 
 //========================================================================
 int main( ){
 
-	ofAppEGLWindow::Settings settings;
+	auto settings = create_settings( 127 );
 
-	settings.eglWindowOpacity = 127;
         settings.frameBufferAttributes[EGL_DEPTH_SIZE]   = 0; // 0 bits for depth
         settings.frameBufferAttributes[EGL_STENCIL_SIZE] = 0; // 0 bits for stencil
 	
-	ofAppEGLWindow window(settings);
+	auto window = make_unique<EGLWindow>( settings );
 
-	ofSetupOpenGL(&window, 1024,768, OF_FULLSCREEN);// <-------- setup the GL context
+	ofSetupOpenGL( std::move(window), 1024, 768, WINDOW_MODE );// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
